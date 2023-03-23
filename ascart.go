9494768c937d2a456409b031d6128f612e36aea0
James Liu <jamesliu96@users.noreply.github.com>
2023-03-23 17:02:59 +0800

    feat: colored

--- ascart.go
+++ ascart.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	_ "image/gif"
 	_ "image/jpeg"
 	"image/png"
+	"io"
 	"log"
 	"math"
 	"math/rand"
@@ -56,8 +56,8 @@ var table = func() (t []byte) {
 
 var lentable = len(table)
 
-func getByte(n uint8) byte {
-	return table[int(float64(n)/math.MaxUint8*float64(lentable-1))]
+func getByte(n uint16) byte {
+	return table[int(float64(n)/math.MaxUint16*float64(lentable-1))]
 }
 
 func drawByte(dst draw.Image, c color.Color, x, y int, s byte) {
@@ -69,31 +69,45 @@ func drawByte(dst draw.Image, c color.Color, x, y int, s byte) {
 	}).DrawBytes([]byte{s})
 }
 
-func main() {
-	src, _, err := image.Decode(os.Stdin)
+func ascart(r io.Reader, w io.Writer, colored bool) (err error) {
+	src, _, err := image.Decode(r)
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 	b := src.Bounds()
 	dx, dy := b.Dx(), b.Dy()
-	dst := image.NewRGBA(b)
-	w, h := basicfont.Face7x13.Advance, basicfont.Face7x13.Height
-	for oy := 0; oy < dy; oy += h {
-		for ox := 0; ox < dx; ox += w {
-			sum, cnt := 0, 0
-			for y := oy; y < oy+h && y < dy; y++ {
-				for x := ox; x < ox+w && x < dx; x++ {
-					sum += int(color.GrayModel.Convert(src.At(x, y)).(color.Gray).Y)
+	dst := image.NewRGBA64(b)
+	W, H := basicfont.Face7x13.Advance, basicfont.Face7x13.Height
+	for oy := 0; oy < dy; oy += H {
+		for ox := 0; ox < dx; ox += W {
+			sumY, sumR, sumG, sumB, cnt := 0, 0, 0, 0, 0
+			for y := oy; y < oy+H && y < dy; y++ {
+				for x := ox; x < ox+W && x < dx; x++ {
+					c := src.At(x, y)
+					y := color.Gray16Model.Convert(c).(color.Gray16)
+					sumY += int(y.Y)
+					if colored {
+						nrgba := color.NRGBA64Model.Convert(c).(color.NRGBA64)
+						sumR += int(nrgba.R)
+						sumG += int(nrgba.G)
+						sumB += int(nrgba.B)
+					}
 					cnt++
 				}
 			}
-			b := getByte(uint8(sum / cnt))
-			drawByte(dst, color.White, ox, oy, b)
-			fmt.Fprintf(os.Stderr, "%c", b)
+			var dstC color.Color = color.White
+			if colored {
+				dstC = color.RGBA64{uint16(sumR / cnt), uint16(sumG / cnt), uint16(sumB / cnt), math.MaxUint16}
+			}
+			drawByte(dst, dstC, ox, oy, getByte(uint16(sumY/cnt)))
 		}
-		fmt.Fprintln(os.Stderr)
 	}
-	if err = png.Encode(os.Stdout, dst); err != nil {
+	err = png.Encode(w, dst)
+	return
+}
+
+func main() {
+	if err := ascart(os.Stdin, os.Stdout, true); err != nil {
 		log.Fatalln(err)
 	}
 }
